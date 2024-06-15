ad596eee7ac254c5cc307627c947f130794d7e56
nayon117 <hasibul.nayon1@gmail.com>
2024-06-15 08:50:56 +0600

    create layout of dashboard

---
+++ components/form/CollectionForm.tsx
@@ -0,0 +1,11 @@
+import { Separator } from "../ui/separator";
+
+const CollectionForm = () => {
+  return (
+    <section className="p-10">
+      <p className="h2-bold text-black">Create Collection</p>
+      <Separator className="mb-7 mt-4 bg-gray-400" />
+    </section>
+  );
+};
+export default CollectionForm;
--- constants/index.ts
+++ constants/index.ts
@@ -24,6 +24,27 @@ export const navbarLinks: NavbarLink[] = [
       label: "Contact",
     },
   ];
+
+
+  export const dashLinks = [
+    {
+      url: "/dashboard",
+      label: "Dashboard",
+    },
+    {
+      url: "/dashboard/collections",
+      label: "Collections",
+    },
+    {
+      url: "/dashboard/orders",
+      label: "Orders",
+    },
+    {
+      url: "/dashboard/customers",
+      label: "Customers",
+    },
+  ];
+
 export const slides: slidesLink[] = [
     {
       imgUrl:"https://i.ibb.co/KLfVdFJ/christopher-gower-m-HRf-Lhg-ABo-unsplash.jpg",
