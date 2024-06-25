72434634f447762111f83f7d383cb7afcffb5b30
nayon117 <hasibul.nayon1@gmail.com>
2024-06-25 09:19:53 +0600

    implement about and testimonial

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,13 @@
+import About from "@/components/shared/About";
 import Banner from "@/components/shared/Banner";
+import Testimonial from "@/components/shared/Testimonial";
 
 const page = () => {
   return (
     <div>
       <Banner />
+      <About/>
+      <Testimonial/>
     </div>
   );
 };
